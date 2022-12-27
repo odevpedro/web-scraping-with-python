0d80c27a3337532d42319fc15befdc2c2bcec297
odevpedro <pedrosschmidt2@gmail.com>
2022-12-27 10:00:23 -0300

    more uses of beautifulsoup

--- firstSoup.py
+++ firstSoup.py
@@ -1,5 +1,42 @@
+import requests
 from bs4 import BeautifulSoup
 
+
+#Web scrapping using a local html file
+'''
 with open("index.html", 'r') as f:
 	soup_string = BeautifulSoup(f.read(), 'html.parser')
 	print(soup_string)
+'''
+
+#now using a external link
+'''
+r = requests.get('http://www.google.com')
+soup = BeautifulSoup(r.text, 'lxml')
+print(soup)
+'''
+
+#using html5lib
+'''
+with open('index.html', 'r') as f: 
+	soup_string = BeautifulSoup(f.read(), 'html5lib')
+print(soup_string)
+'''
+
+#acess a specific tag in html file
+'''
+with open('index.html', 'r') as f: 
+	soup = BeautifulSoup(f, 'lxml')
+
+tag = soup.title # that is valid for any tag that is put after
+print(tag)
+'''
+
+
+
+#getting into a tag
+with open('index.html', 'r') as f: 
+	soup = BeautifulSoup(f, 'lxml')
+
+tag = soup.p.get_text()
+print(tag)
